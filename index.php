e68f8dce4e3d7b37b4723c70d4d18a362b2ce7b5
nadiaaamand <nadiaaamand@gmail.com>
2018-12-18 09:28:55 +0100

    installed sass and bootstrap
    Created a index.php file and installed bootstrap 4 and sass. Download Koala so you can also compile the files.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<!--Bootstrap CSS-->
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+<!--Main SCSS-->
+<link rel="stylesheet" href="scss/main.scss">
+
+<!--Bootstrap JS-->
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<meta charset="UTF-8">
+<title>FICS Administration</title>
+</head>
+
+<body>
+</body>
+</html>
