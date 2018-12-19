14059bc6f6c92b7a12276ed335fee39d37b06c04
nadiaaamand <nadiaaamand@gmail.com>
2018-12-19 14:29:58 +0100

    Søg og opret sag lavet
    Udseende til opret sag og søg er lavet.

--- opretsag.php
+++ opretsag.php
@@ -15,5 +15,247 @@
 </head>
 
 <body>
+
+<div class="container col-10 p-0 border">
+<!--Main Navigation-->
+	<nav class="nav border-bottom">
+	  <a class="nav-link" href="opretsag.php">Værktøjslinje</a>
+	  <a class="nav-link" href="soeg.php">Skabeloner</a>
+	  <a class="nav-link" href="#">Hjælp</a>
+	</nav>
+	
+	<nav class="nav nav-pills nav-justified border-bottom">
+	  <a class="nav-link active" href="#">Sagsdata</a>
+	  <a class="nav-link box" href="#">Emneord</a>
+	  <a class="nav-link box" href="#">Ejendom</a>
+	  <a class="nav-link box" href="#">Bemærkning</a>
+	</nav>
+	
+	<div class="container-fluid pt-4">
+		<form>
+	  <!--Row 1 -->
+		  <div class="row">
+			<div class="col">
+			<div class="form-group row">
+				<label class="col-3">Journalplan</label>
+				<div class="col-9">
+				<input type="text" class="form-control" name="journalplan">
+				</div>
+			</div>
+			</div>
+			
+			<div class="col">
+			 	 <div class="form-group row">
+					<label class="col-3">Sagstype</label>
+					<div class="col-9">
+					<input type="text" class="form-control" name="journalplan">
+					</div>
+			</div>
+			</div>
+			
+			 </div>
+			
+			<!--Row 2 -->
+			<div class="row">
+				<div class="col">
+			<div class="form-group row">
+				<label class="col-3">Stikord</label>
+				<div class="col-9">
+				<input type="text" class="form-control" name="journalplan">
+				</div>
+			</div>
+			</div>
+			
+			<div class="col">
+			 	 <div class="form-group row">
+					<label class="col-3">Sagsid</label>
+					<div class="col-9">
+					<input type="text" class="form-control" name="journalplan">
+					</div>
+			</div>
+			</div>
+		 </div>
+		 
+		 <!--Row 3 -->
+			<div class="row">
+				<div class="col">
+			<div class="form-group row">
+				<label class="col-3">Sagsgruppe</label>
+				<div class="col-9">
+				<input type="text" class="form-control" name="journalplan">
+				</div>
+			</div>
+			</div>
+			
+			<div class="col">
+			 	 <div class="form-group row">
+					<label class="col-3">Opr. dato</label>
+					<div class="col-9">
+					<input type="text" class="form-control" name="journalplan">
+					</div>
+			</div>
+			</div>
+		 </div>
+		 
+		 <!--Row 4 -->
+			<div class="row">
+				<div class="col">
+			<div class="form-group row">
+				<label class="col-3">Undernr.</label>
+				<div class="col-9">
+				<input type="text" class="form-control" name="journalplan">
+			  <div class="form-check pt-2">
+				  <input class="form-check-input" type="checkbox" id="gridCheck">
+					  <label class="form-check-label" for="gridCheck">
+						Advar ved undernr. på gruppen
+					  </label>
+					</div>
+				</div>
+			</div>
+			</div>
+			
+			<div class="col">
+			 	 <div class="form-group row">
+					<label class="col-3">Klassifikation</label>
+					<div class="col-9">
+					<input type="text" class="form-control" name="journalplan">
+					</div>
+			</div>
+			</div>
+		 </div>
+		 
+		 <!--Row 5 -->
+			<div class="row">			
+			<div class="col">
+			 	 <div class="form-group row offset-md-6">
+					<label class="col-3">Tilføjelse</label>
+					<div class="col-9">
+					<div class="form-check">
+				  <input class="form-check-input" type="checkbox" id="gridCheck">
+					  <label class="form-check-label" for="gridCheck">
+						Workflow
+					  </label>
+					</div>
+					</div>
+			</div>
+			</div>
+		 </div>
+		 
+		 <!--Row 6 - Sagssavne -->
+			<div class="row pt-4">
+				<div class="col">
+			<div class="form-group row">
+				<label class="col-3">Sagsnavn 1</label>
+				<div class="col-9">
+				<input type="text" class="form-control" name="journalplan">
+				</div>
+			</div>
+			</div>
+			
+			<div class="col">
+			 	 <div class="form-group row">
+					<label class="col-3">Sagsnavn 3</label>
+					<div class="col-9">
+					<input type="text" class="form-control" name="journalplan">
+					</div>
+			</div>
+			</div>
+		 </div>
+		 
+		 <!--Row 7 - Sagssavne -->
+			<div class="row">
+				<div class="col">
+			<div class="form-group row">
+				<label class="col-3">Sagsnavn 2</label>
+				<div class="col-9">
+				<input type="text" class="form-control" name="journalplan">
+				</div>
+			</div>
+			</div>
+			
+			<div class="col">
+			 	 <div class="form-group row">
+					<label class="col-3">Sagsnavn 4</label>
+					<div class="col-9">
+					<input type="text" class="form-control" name="journalplan">
+					</div>
+			</div>
+			</div>
+		 </div>
+		 
+		 <!--Row 8 generel info -->
+			<div class="row pt-5">
+				<div class="col">
+			<div class="form-group row">
+				<label class="col-3">Medarbejder</label>
+				<div class="col-9">
+				<input type="text" class="form-control" name="journalplan">
+				</div>
+			</div>
+			</div>
+			
+			<div class="col">
+			 	 <div class="form-group row">
+					<label class="col-3">Sagsafdeling</label>
+					<div class="col-9">
+					<input type="text" class="form-control" name="journalplan">
+					</div>
+			</div>
+			</div>
+		 </div>
+		 
+		 <!--Row 9 generel info -->
+			<div class="row">
+				<div class="col">
+			<div class="form-group row">
+				<label class="col-3">Afdeling</label>
+				<div class="col-9">
+				<input type="text" class="form-control" name="journalplan">
+				</div>
+			</div>
+			</div>
+			
+			<div class="col">
+			 	 <div class="form-group row">
+					<label class="col-3">Status</label>
+					<div class="col-9">
+					<input type="text" class="form-control" name="journalplan">
+					</div>
+			</div>
+			</div>
+		 </div>
+		 
+		 <!--Row 10 generel info -->
+			<div class="row">
+				<div class="col">
+			<div class="form-group row">
+				<label class="col-3">Sagsansvarlig</label>
+				<div class="col-9">
+				<input type="text" class="form-control" name="journalplan">
+				</div>
+			</div>
+			</div>
+			
+			<div class="col">
+			 	 <div class="form-group row">
+					<label class="col-3">Adresse</label>
+					<div class="col-9">
+					<input type="text" class="form-control" name="journalplan">
+					</div>
+			</div>
+			</div>
+		 </div>
+			<div class="form-group row pt-3">
+				<div class="offset-md-10">
+			  		<button class="btn btn-secondary">Ryd</button>
+			  		<button type="submit" class="btn btn-primary">Opret sag</button>
+				</div>
+		  </div>
+		   
+		 
+		</form>
+	</div>
+	
+		</div>
 </body>
 </html>
--- soeg.php
+++ soeg.php
@@ -15,5 +15,155 @@
 </head>
 
 <body>
+<div class="container col-10 p-0 border">
+<!--Main Navigation-->
+	<nav class="nav border-bottom">
+	  <a class="nav-link" href="opretsag.php">Moduler</a>
+	  <a class="nav-link" href="soeg.php">Funktioner</a>
+	</nav>
+	
+	<nav class="nav nav-pills nav-justified border-bottom">
+	  <a class="nav-link soeg active" href="#">Søgning 1<img class="sletfane" src="img/sletfane.png" alt="luk fane"></a>
+	  <a class="nav-link soeg" href="#">Søgning 2<img class="sletfane" src="img/sletfane.png" alt="luk fane"></a>
+	  <a class="nav-link" href="#"><img class="addtab" src="img/plus.png" alt="tilføj søgning"></a>
+	</nav>
+	
+	<!--Search-->
+	<div class="container-fluid pt-4">
+		<form>
+	  <!--Row 1 -->
+		  <div class="row">
+			<div class="col">
+			<div class="form-group row">
+				<label class="col-3">Område</label>
+				<div class="col-9">
+				<input type="text" class="form-control" name="journalplan">
+				</div>
+			</div>
+			</div>
+			
+			<div class="col">
+			 	 <div class="form-group row">
+					<label class="col-3">Medarbejder</label>
+					<div class="col-9">
+					<input type="text" class="form-control" name="journalplan">
+					</div>
+			</div>
+			</div>
+			
+			 </div>
+			
+			<!--Row 2 -->
+			<div class="row">
+				<div class="col">
+			<div class="form-group row">
+				<label class="col-3">Journalnummer</label>
+				<div class="col-9">
+				<input type="text" class="form-control" name="journalplan">
+				</div>
+			</div>
+			</div>
+			
+			<div class="col">
+			 	 <div class="form-group row">
+					<label class="col-3">Afdeling</label>
+					<div class="col-9">
+					<input type="text" class="form-control" name="journalplan">
+					</div>
+			</div>
+			</div>
+			</div>
+			
+			<!--Row 3 -->
+			<div class="row">
+				<div class="col">
+			<div class="form-group row">
+				<label class="col-3">Sagsid</label>
+				<div class="col-9">
+				<input type="text" class="form-control" name="journalplan">
+				</div>
+			</div>
+			</div>
+			
+			<div class="col">
+			 	 <div class="form-group row">
+					<label class="col-3">Sagsnavn</label>
+					<div class="col-9">
+					<input type="text" class="form-control" name="journalplan">
+					</div>
+			</div>
+			</div>
+			</div>
+			
+			<!--Row 4 -->
+			<div class="row">
+				<div class="col">
+			<div class="form-group row">
+				<label class="col-3">Sagsnummer</label>
+				<div class="col-9">
+				<input type="text" class="form-control" name="journalplan">
+				</div>
+			</div>
+			</div>
+			
+			<div class="col">
+			 	 <div class="form-group row">
+					<label class="col-3">Sagstype</label>
+					<div class="col-9">
+					<input type="text" class="form-control" name="journalplan">
+					</div>
+			</div>
+			</div>
+			</div>
+			
+			<!--Row 5 -->
+			<div class="row">
+				<div class="col">
+			<div class="form-group row">
+				<label class="col-3">Emneord</label>
+				<div class="col-9">
+				<input type="text" class="form-control" name="journalplan">
+				</div>
+			</div>
+			</div>
+			
+			<div class="col">
+						<div class="form-check form-check-inline">
+						  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+						  <label class="form-check-label pl-1" for="inlineCheckbox1">Bilagstekst</label>
+						</div>
+						<div class="form-check form-check-inline">
+						  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+						  <label class="form-check-label pl-1" for="inlineCheckbox2">Dokumenter</label>
+						</div>
+						<div class="form-check form-check-inline">
+						  <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3">
+						  <label class="form-check-label pl-1" for="inlineCheckbox3">Sager</label>
+						</div>
+			</div>
+			
+			</div>
+				
+						 <!--Row 6 -->
+			<div class="row">			
+			<div class="col">
+			 	 <div class="row">
+					<a class="col-12" href="#">Flere søgemuligheder<img class="pil" src="img/pil.png" alt="fleresøgemuligheder"></a>
+					
+			</div>
+			</div>
+		 </div>
+					<div class="form-group row pt-3 pb-2">
+						<div class="offset-md-9 pl-5">
+							<button class="btn btn-secondary ml-2">Ryd</button>
+							<button type="submit" class="btn btn-primary pl-5 pr-5 ml-1">Søg</button>
+						</div>
+		  </div>
+		   
+			</div>
+			
+			</div>
+	
+	</div>
 </body>
 </html>
